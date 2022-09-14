6ed0b47966826cd578d16dace0a1197717ae6957
Thelarion <46968231+Thelarion@users.noreply.github.com>
2022-09-14 19:43:37 +0200

    OculusSpatializer integration

--- Assets/Scripts/Music Elements/Drums/DrumSample.cs
+++ Assets/Scripts/Music Elements/Drums/DrumSample.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class DrumSample : MonoBehaviour
 {
   public FMODUnity.EventReference _eventPath;
+  public int _windowSize = 512;
+  public FMOD.DSP_FFT_WINDOW _windowShape = FMOD.DSP_FFT_WINDOW.RECT;
   public void OnCollisionEnter() {
     FMODUnity.RuntimeManager.PlayOneShot(_eventPath, transform.position);
   }
