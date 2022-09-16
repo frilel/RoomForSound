32b50c40bd267b51814242b8f15de76c364ff6a5
Christian Lindberg <59727728+frilel@users.noreply.github.com>
2022-09-16 15:31:01 +0200

    add joints to cymbals

--- Assets/Scripts/Music Elements/Drums/DrumSample.cs
+++ Assets/Scripts/Music Elements/Drums/DrumSample.cs
@@ -11,7 +11,9 @@ public class DrumSample : MonoBehaviour
         if (collision.transform.TryGetComponent<DrumStick>(out DrumStick drumStick))
         {
             impactSpeed = Vector3.Distance(drumStick.previousPos, drumStick.transform.position) / Time.deltaTime;
-            Debug.Log("You hit the drum with impact speed: " + impactSpeed);
+#if UNITY_EDITOR
+            Debug.Log("You hit the" + transform.name + "with impact speed: " + impactSpeed);
+#endif
         }
 
         FMODUnity.RuntimeManager.PlayOneShot(_eventPath, transform.position);
