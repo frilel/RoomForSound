75110fdd7d387cb7e4cf337d08cd73badec29fc4
Lonerangerix <1137198072@qq.com>
2022-09-18 23:29:25 +0200

    ss

--- Assets/Scripts/Music Elements/Drums/DrumSample.cs
+++ Assets/Scripts/Music Elements/Drums/DrumSample.cs
@@ -24,30 +24,30 @@ public class DrumSample : MonoBehaviour
             // calculate the impact speed
             impactSpeed = Vector3.Distance(drumStick.previousPos, drumStick.transform.position) / Time.deltaTime;
 
-        #if UNITY_EDITOR
+            //#if UNITY_EDITOR
 
             // normalize the impact speed to get a range approx. between 0 and 1
             float normalizedImpactSpeed = impactSpeed / 2;
             // limit the min value to 0 and max value to 1 
-            float clampImpactSpeed = Mathf.Clamp(normalizedImpactSpeed,0,1);
+            float clampImpactSpeed = Mathf.Clamp(normalizedImpactSpeed, 0, 1);
             // feedback to console
             Debug.Log("You hit the" + transform.name + "with impact speed: " + clampImpactSpeed);
-        
-        #endif
 
-        drumHitSFXInstance = FMODUnity.RuntimeManager.CreateInstance(eventPath);
-        drumHitSFXInstance.setParameterByName("Pitch", clampImpactSpeed);
-        drumHitSFXInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
-        drumHitSFXInstance.start();
-        drumHitSFXInstance.release();
+            //#endif
+
+            drumHitSFXInstance = FMODUnity.RuntimeManager.CreateInstance(eventPath);
+            drumHitSFXInstance.setParameterByName("Pitch", clampImpactSpeed);
+            drumHitSFXInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+            drumHitSFXInstance.start();
+            drumHitSFXInstance.release();
         }
         //FMODUnity.RuntimeManager.PlayOneShot(eventPath, transform.position);
-        if(_VFXController != null) 
+        if (_VFXController != null)
         {
             _VFXController.triggerOne(collision.transform);
             _VFXController.triggerVibration(OVRInput.Controller.RTouch, 0.1f, 0.1f, 1);
         }
-        if (drumStick!=null)
+        if (drumStick != null)
         {
             //_VFXController.triggerVibration(drumStick.getGrabber(), 0.1f, 0.1f, 1);
         }
