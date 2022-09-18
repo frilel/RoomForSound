fd271735c45a35c0ca786e73fb4ffe63d04b8e0d
Lonerangerix <1137198072@qq.com>
2022-09-18 21:37:52 +0200

    Audience Different actions implementation

--- Assets/AudienceControl.cs
+++ Assets/AudienceControl.cs
@@ -9,6 +9,8 @@ public class AudienceControl : MonoBehaviour
     public Text messageText;
     public GameObject messagePanel;
     public Audience audience;
+    public Animator animator;
+    private int avatarIndex;
     // Start is called before the first frame update
     void Start()
     {
@@ -41,33 +43,79 @@ public class AudienceControl : MonoBehaviour
     }
     public void UpdateMessage()
     {
-        Debug.Log("got message from "+audience.name+": "+audience.messages[0].message);
-        messagePanel.SetActive(true);
-        messageText.text = audience.messages[0].message;
+        string text = audience.messages[0].message;
+        Debug.Log("got message from " + audience.name + ": " + text);
         StartCoroutine("DeleteMessage");
+        switch (text)
+        {
+            case "Dance":
+            case "dance":
+                animator.Play("Dance");
+                break;
+            case "Clap":
+            case "clap":
+                animator.Play("Clap");
+                break;
+            case "Idle":
+            case "idle":
+                if (avatarIndex == 0)
+                {
+                    animator.Play("FemaleIdle");
+                }
+                else
+                {
+                    animator.Play("MaleIdle");
+                }
+                break;
+            case "Wave":
+            case "wave":
+                animator.Play("Wave");
+                break;
+            case "Cheer":
+            case "cheer":
+                animator.Play("Cheer");
+                break;
+
+            default:
+                break;
+        }
+        messagePanel.SetActive(true);
+        messageText.text = text;
+
     }
     IEnumerator DeleteMessage()
     {
         yield return new WaitForSeconds(2);
         messagePanel.SetActive(false);
-        string url="https://roomforsound-server.herokuapp.com/messages?id="+audience.messages[0].id;
+        string url = "https://roomforsound-server.herokuapp.com/messages?id=" + audience.messages[0].id;
         UnityWebRequest www = UnityWebRequest.Delete(url);
         Debug.Log("Message Deleted");
         yield return www.SendWebRequest();
     }
     public void ChangeAvatar(int index)
     {
+        avatarIndex = index;
         for (int i = 0; i < 4; i++)
         {
             if (i == index)
             {
                 transform.GetChild(i).gameObject.SetActive(true);
+                animator = transform.GetChild(i).GetChild(1).GetComponent<Animator>();
+
             }
             else
             {
                 transform.GetChild(i).gameObject.SetActive(false);
             }
 
+        }
+        if (avatarIndex == 0)
+        {
+            animator.Play("FemaleIdle");
+        }
+        else
+        {
+            animator.Play("MaleIdle");
         }
     }
 
