ad5c3ec24919253d572b2b31cbebc9cb983136ae
Christian Lindberg <59727728+frilel@users.noreply.github.com>
2022-09-21 22:00:30 +0200

    update drumstick.cs
    removed debug logs

--- Assets/Scripts/Music Elements/Drums/DrumStick.cs
+++ Assets/Scripts/Music Elements/Drums/DrumStick.cs
@@ -6,28 +6,23 @@ public class DrumStick : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.LogError("Drumstick Collision");
         if (collision.transform.name == GameManager.Instance.LeftHandControllerRoot.transform.name)
         {
             usedController = OVRInput.Controller.LTouch;
-            Debug.LogError("LEFT");
         }
         else if (collision.transform.name == GameManager.Instance.RightHandControllerRoot.transform.name)
         {
             usedController = OVRInput.Controller.RTouch;
-            Debug.LogError("RIGHT");
         }
     }
     private void OnCollisionExit(Collision collision)
     {
         if (collision.transform.name == GameManager.Instance.LeftHandControllerRoot.transform.name)
         {
-            Debug.LogError("NO LEFT");
             usedController = OVRInput.Controller.None;
         }
         else if (collision.transform.name == GameManager.Instance.RightHandControllerRoot.transform.name)
         {
-            Debug.LogError("NO RIGHT");
             usedController = OVRInput.Controller.None;
         }
     }
