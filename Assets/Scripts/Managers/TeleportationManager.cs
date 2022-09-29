e25cf2b175fd2897207568ff356c20e96e127063
Christian Lindberg <59727728+frilel@users.noreply.github.com>
2022-09-29 19:44:33 +0200

    work on lighting MergedScene1
    +disabled mesh renderers for hitSurfaces on DrumSurfacesSeparatedChristian.prefab

--- Assets/Scripts/Managers/TeleportationManager.cs
+++ Assets/Scripts/Managers/TeleportationManager.cs
@@ -37,8 +37,8 @@ public class TeleportationManager : MonoBehaviour
     void SetupTeleportDefaults()
     {
         locomotionTeleport.enabled = true;
-        locomotionTeleport.EnableMovement(false, false, false, false);
-        locomotionTeleport.EnableRotation(false, false, false, false);
+        //locomotionTeleport.EnableMovement(false, false, false, false);
+        //locomotionTeleport.EnableRotation(true, false, false, true);
 
         var input = locomotionTeleport.GetComponent<TeleportInputHandlerTouch>();
         input.AimingController = AimingController;
